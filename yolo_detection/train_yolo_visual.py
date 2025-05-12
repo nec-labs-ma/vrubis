b09f341eb3242528cefabd1c2373f3f2f8c797e4
Sparsh Garg <sparsh@nec-labs.com>
2025-05-12 12:08:55 -0700

    added yolo detection model

---
+++ yolo_detection/train_yolo_visual.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+
+# Load a pretrained YOLO11n model
+model = YOLO("yolo11n.pt")
+
+# Train the model on the COCO8 dataset for 100 epochs
+train_results = model.train(
+    data="usdot_visual.yaml",  # Path to dataset configuration file
+    epochs=100,  # Number of training epochs
+    imgsz=1280,  # Image size for training
+    device=[0,1,2,3],  # Device to run on (e.g., 'cpu', 0, [0,1,2,3])
+    batch=64,
+    pretrained=True
+)
+
+metrics = model.val()
+print(metrics)
+
